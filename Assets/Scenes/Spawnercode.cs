32eac666bb69fdbe765d57ddebb93b7a1714376e
rahul02122000 <ra2777@srmist.edu.in>
2023-10-11 19:50:03 -0700

    rahul collectibles disappear code

--- Assets/Scenes/Spawnercode.cs
+++ Assets/Scenes/Spawnercode.cs
@@ -1,3 +1,5 @@
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +7,15 @@ using UnityEngine;
 public class Spawnercode : MonoBehaviour
 {
     public GameObject[] myobj;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             int randomIndex = Random.Range(0, myobj.Length);
             Vector3 randomSpawn = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), -1);
 
+            // This line will instantiate a capsule and the capsule will start its own deactivation timer.
             Instantiate(myobj[randomIndex], randomSpawn, Quaternion.identity);
         }
     }
