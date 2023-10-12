c486bd4fa5af0b4ff7a239f0f198c949d97f7aae
Aviral Goel <aviralgoel@rocketmail.com>
2023-10-12 16:20:55 -0700

    player collision detection
    somewhat working

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CollisionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // detect collision with other game bodies 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (this.gameObject.CompareTag("Player1Blade") && collision.gameObject.CompareTag("Player2"))
+        {
+            Debug.Log("Player 1 hit Player 2");
+        }
+        else if (this.gameObject.CompareTag("Player2Blade") && collision.gameObject.CompareTag("Player1"))
+        {
+            Debug.Log("Player 2 hit Player 1");
+        }
+    }
+    private void Update()
+    {
+        // if space key is pressed, print in debug the tag of this game object
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Debug.Log(this.gameObject.tag);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
