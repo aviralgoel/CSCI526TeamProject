b62bba8557a6fe140b3dd95b47693a1c82346627
Shweta Kumari <shwetajnct4@gmail.com>
2023-10-12 18:59:05 -0700

    Adding Player Analytic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,6 +24,11 @@ public class GameManager : MonoBehaviour
     // player 2 variables
     bool isPlayerTwoActive = false;
 
+    public string player1StartTime;
+    public string player1EndTime;
+    public string player2StartTime;
+    public string player2EndTime;
+
     
 
     // Start is called before the first frame update
@@ -37,18 +42,25 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        AnalyticsCollector analyticsCollector = GetComponent<AnalyticsCollector>();
         if (!isPlayerOneActive)
         {
             if (Input.GetKeyDown(KeyCode.A))
             {
+                float temp = Time.time;
+                player1StartTime = temp.ToString(); 
                 isPlayerOneActive=true;
+                analyticsCollector.SendPlayer1("A", player1StartTime);
             }
         }
         if (!isPlayerTwoActive)
         {
             if (Input.GetKeyDown(KeyCode.L))
             {
+                float temp = Time.time;
+                player2StartTime = temp.ToString(); 
                 isPlayerTwoActive=true;
+                analyticsCollector.SendPlayer2("L", player2StartTime);
             }
         }
     }
