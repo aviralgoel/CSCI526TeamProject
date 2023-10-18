c8218c839d61ecae687a6ee7fb894ba4ba132f53
Aviral Goel <aviralgoel@rocketmail.com>
2023-10-18 09:11:04 -0700

    updated ui text and final touches

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,27 +38,27 @@ public class GameManager : MonoBehaviour
 
         if (!isPlayerOneActive) // player has not yet joined the game
         {
-            if (Input.GetKeyDown(KeyCode.A)) // player has joined the game
+            if (Input.GetKeyDown(KeyCode.L)) // player has joined the game
             {
                 float temp = Time.time;
                 player1StartTime = temp.ToString(); 
                 isPlayerOneActive=true;
                 analyticsCollector.SendPlayer1("A", player1StartTime);
-                playerOne.GetComponent<ScoreManager>().SetPlayerActive(true);
-                playerOne.GetComponent<ScoreManager>().SetPlayerNumber(1);
+                player1ScoreManager.SetPlayerActive(true);
+                player1ScoreManager.SetPlayerNumber(1);
                 playerOne.GetComponent<PlayerInputController>().isMovementAllowed = true; 
             }
         }
         if (!isPlayerTwoActive) // player has not yet joined the game
         {
-            if (Input.GetKeyDown(KeyCode.L)) // player has joined the game
+            if (Input.GetKeyDown(KeyCode.A)) // player has joined the game
             {
                 float temp = Time.time;
                 player2StartTime = temp.ToString(); 
                 isPlayerTwoActive=true;
                 analyticsCollector.SendPlayer2("L", player2StartTime);
-                playerTwo.GetComponent<ScoreManager>().SetPlayerActive(true);
-                playerTwo.GetComponent<ScoreManager>().SetPlayerNumber(2);
+                player2ScoreManager.SetPlayerActive(true);
+                player2ScoreManager.SetPlayerNumber(2);
                 playerTwo.GetComponent<PlayerInputController>().isMovementAllowed = true;
             }
         }
