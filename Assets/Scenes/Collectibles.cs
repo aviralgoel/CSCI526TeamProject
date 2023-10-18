9e30caa6fbd0628065af96e940d74a5e9be5bf26
Aviral Goel <aviralgoel@rocketmail.com>
2023-10-17 21:00:39 -0700

    tom scene has rectangle fix

--- Assets/Scenes/Collectibles.cs
+++ Assets/Scenes/Collectibles.cs
@@ -73,52 +73,52 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
-    public float speed;
-    public Vector3 direction; 
-    bool running = false;
+    //public float speed;
+    //public Vector3 direction; 
+    //bool running = false;
 
-    public float minDeactivationTime = 3f;
-    public float maxDeactivationTime = 15f;
-    Vector3 dest;
+    //public float minDeactivationTime = 3f;
+    //public float maxDeactivationTime = 15f;
+    //Vector3 dest;
     
     void Start()
     {
-        direction = Vector3.zero;
+        //direction = Vector3.zero;
     }
 
-    private void OnEnable()
+  /*  private void OnEnable()
     {
-        StartCoroutine(DelayedDeactivate());
-    }
+        //StartCoroutine(DelayedDeactivate());
+    }*/
 
     void Update()
     {
-        if (!running)
+       /* if (!running)
         {
             StartCoroutine(changeDirection());
         }
         dest = transform.position + direction * 0;
-        transform.position = Vector3.Lerp(transform.position, dest, Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, dest, Time.deltaTime);*/
     }
 
-    IEnumerator changeDirection()
+/*    IEnumerator changeDirection()
     {
         running = true;
         yield return new WaitForSeconds(0.5f);
         direction.x = Random.Range(-2.5f, 3.75f);
         direction.y = Random.Range(-2.1f, 3.5f);
         running = false;
-    }
-
+    }*/
+/*
     IEnumerator DelayedDeactivate()
     {
         float randomTime = Random.Range(minDeactivationTime, maxDeactivationTime);
         yield return new WaitForSeconds(randomTime);
         this.gameObject.SetActive(false);
-    }
+    }*/
 
     // Updated collision method with debug log messages for "Player1" and "Player2"
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
         {
