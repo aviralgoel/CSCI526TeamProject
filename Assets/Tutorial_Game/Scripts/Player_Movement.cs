2b9bdbd5e3bc197799894c33f0657db6c14267c2
SHARAN MURLI <murli@usc.edu>
2023-10-24 23:21:14 -0700

    Tutorial Scene for Player Join

--- Assets/Tutorial_Game/Scripts/Player_Movement.cs
+++ Assets/Tutorial_Game/Scripts/Player_Movement.cs
@@ -1,54 +1,93 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Vector3 direction;
-    private Quaternion targetRotation;
-    private float turnSpeed = 1f;
-    private float turnSpeedMultiplier = 1f; // Declare turnSpeedMultiplier here
-    private KeyCode controllingKey;
-
-    public float movementSpeed = 1f;
+    int playerNumber;
+    Rigidbody2D rb;
+    Vector3 direction;
+    Quaternion targetRotation;
+    public bool isMovementAllowed;
+    public float movementSpeed;
+    public float turnSpeed;
     public float angleToTurn = 10f;
-    public float speedMultiplier = 1f;
-
+    private ScoreManager scoreManager;
+    [Range(1f, 5f)]
+    public float speedMultiplier;
+    [Range(0f, 2f)]
+    public float turnSpeedMultiplier;
+    
+    KeyCode controllingKey;
     private float defaultTurnSpeedMultiplierValue = 1f;
 
-    private void Start()
+    //public GameObject blackHole;
+
+    // Start is called before the first frame update
+    void Start()
     {
-    rb = GetComponent<Rigidbody2D>();
-
-
-        // Determine the controlling key based on the tag of the GameObject.
-        controllingKey = gameObject.CompareTag("Player1") ? KeyCode.L : KeyCode.A;
+        if (gameObject.tag == "Player1")
+        {
+            playerNumber = 1;
+        }
+        else if (gameObject.tag == "Player2")
+        {
+            playerNumber = 2;
+        }
+        rb = GetComponent<Rigidbody2D>();
+        controllingKey = playerNumber == 1 ? KeyCode.L : KeyCode.A;
+        rb.velocity = Vector3.zero;
+        isMovementAllowed = false;
+        speedMultiplier = 1f;
+        turnSpeedMultiplier = 1f;
+        turnSpeed = 1f;
+        scoreManager = GetComponent<ScoreManager>();
+        
     }
+
+    // Update is called once per frame
 
     private void FixedUpdate()
+    {   
+        if(isMovementAllowed)
+        {
+            InputController();
+        }
+        
+
+    }
+    private void Update()
     {
-        InputController();
+        speedMultiplier = (scoreManager.GetTimeActive() < 10f) ? 1f : 1 + scoreManager.GetTimeActive() / 75f;
+        angleToTurn = (scoreManager.GetTimeActive() < 10f) ? 10f : 10f + scoreManager.GetTimeActive() / 20f;
+        //defaultTurnSpeedMultiplierValue = (scoreManager.GetTimeActive() < 10f) ? 1f : 1 + scoreManager.GetTimeActive() / 10f;
     }
 
-    private void InputController()
-    {
-        rb.velocity = transform.up * movementSpeed * Time.deltaTime * speedMultiplier;
 
+    private void InputController()
+    {   
+        // turning and moving the player always to its right
+        // using forces (and not trasnform.Translate())
+      
+        rb.velocity = transform.up * movementSpeed * Time.deltaTime * speedMultiplier;
         if (Input.GetKey(controllingKey))
         {
-            direction = Quaternion.Euler(0, 0, angleToTurn * turnSpeed) * transform.up * Time.deltaTime;
+           
+            direction = Quaternion.Euler(3, 5, angleToTurn * turnSpeed) * transform.up * Time.deltaTime;
             turnSpeed += turnSpeedMultiplier * Time.deltaTime;
         }
         else
         {
+            // direction = transform.position - blackHole.transform.position;
             turnSpeed = 1f;
-            direction = Quaternion.Euler(0, 0, -angleToTurn * turnSpeed) * transform.up * Time.deltaTime;
+            direction = Quaternion.Euler(3, 5, -angleToTurn*turnSpeed) * transform.up * Time.deltaTime;
         }
         targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
-
-        if (Input.GetKeyUp(controllingKey))
+        
+        if(Input.GetKeyUp(controllingKey))
         {
             turnSpeedMultiplier = defaultTurnSpeedMultiplierValue;
         }
     }
+   
 }
