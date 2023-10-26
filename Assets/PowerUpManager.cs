a8958a843841513d0204b7ee9368751a0ffed48e
Aviral Goel <aviralgoel@rocketmail.com>
2023-10-26 13:39:49 -0700

    firewall mechanism done
    have to fix respawn bug
    have to fix health reduce bug

--- Assets/PowerUpManager.cs
+++ Assets/PowerUpManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -9,7 +10,35 @@ public class PowerUpManager : MonoBehaviour
     [SerializeField] private int playerNumber;
     [SerializeField] private KeyCode PowerUpControllingKey;
     [SerializeField] private int totalPowerUpCount = 0;
-    public GameObject wallBottom;
+
+    public float fireWallMovementSpeed = 0.2f;
+
+    public Transform wallBottom;
+    public Transform wallTop;
+    public Transform wallLeftTop;
+    public Transform wallLeftBottom;
+    public Transform wallRightTop;
+    public Transform wallRightBottom;
+
+    public Transform wallBottomDestination;
+    public Transform wallTopDestination;
+    public Transform wallLeftTopDestination;
+    public Transform wallLeftBottomDestination;
+    public Transform wallRightTopDestination;
+    public Transform wallRightBottomDestination;
+
+    public Transform wallBottomSource;
+    public Transform wallTopSource;
+    public Transform wallLeftTopSource;
+    public Transform wallLeftBottomSource;
+    public Transform wallRightTopSource;
+    public Transform wallRightBottomSource;
+
+
+
+    public bool fireWallActive = false;
+    public bool moveWallsInside = false;
+    public bool moveWallsOutside = false;
     // enum for powerup types
     public enum PowerUpType
     {
@@ -30,6 +59,14 @@ public class PowerUpManager : MonoBehaviour
     {
         playerNumber = GetComponent<ScoreManager>().GetPlayerNumber();
         PowerUpControllingKey = (playerNumber == 2) ? KeyCode.Q : KeyCode.P;
+
+        /*wallBottomSource = wallBottom.position;
+        wallTopSource = wallTop.position;
+        wallLeftTopSource = wallLeftTop.position;
+        wallLeftBottomSource = wallLeftBottom.position;
+        wallRightTopSource = wallRightTop.position;
+        wallRightBottomSource = wallRightBottom.position;*/
+
     }
 
     // Update is called once per frame
@@ -39,6 +76,64 @@ public class PowerUpManager : MonoBehaviour
         {
             UsePowerUp();
         }
+        if(fireWallActive == true && moveWallsInside == true)
+        {
+            MoveWallsInside();
+        }
+        if(fireWallActive == true && moveWallsOutside == true)
+        {
+            MoveWallsOutside();
+        }
+    }
+
+    private void MoveWallsInside()
+    {   
+        // move
+        wallBottom.position = Vector3.MoveTowards(wallBottom.position, wallBottomDestination.position, Time.deltaTime * fireWallMovementSpeed);
+        wallLeftBottom.position = Vector3.MoveTowards(wallLeftBottom.position, wallLeftBottomDestination.position, Time.deltaTime*fireWallMovementSpeed);
+        wallLeftTop.position = Vector3.MoveTowards(wallLeftTop.position, wallLeftTopDestination.position, Time.deltaTime*fireWallMovementSpeed);
+        wallRightBottom.position = Vector3.MoveTowards(wallRightBottom.position, wallRightBottomDestination.position, Time.deltaTime*fireWallMovementSpeed);
+        wallRightTop.position = Vector3.MoveTowards(wallRightTop.position, wallRightTopDestination.position, Time.deltaTime*fireWallMovementSpeed);
+        wallTop.position = Vector3.MoveTowards(wallTop.position, wallTopDestination.position, Time.deltaTime*fireWallMovementSpeed);
+        if (Mathf.Approximately(Vector3.Distance(wallBottom.position, wallBottomDestination.position),0))
+        {
+            StartCoroutine(Pause());
+            moveWallsInside = false;
+
+        }
+        
+
+        // shake
+        //wallBottom.GetComponent<ShakeMe>().Shake();
+        //wallLeftBottom.GetComponent<ShakeMe>().Shake();
+        //wallLeftTop.GetComponent<ShakeMe>().Shake();
+        //wallRightBottom.GetComponent<ShakeMe>().Shake();
+        //wallRightTop.GetComponent<ShakeMe>().Shake();
+        //wallTop.GetComponent<ShakeMe>().Shake();
+    }
+    private void MoveWallsOutside()
+    {   
+        // move
+        wallBottom.position = Vector3.MoveTowards(wallBottom.position, wallBottomSource.position, Time.deltaTime * fireWallMovementSpeed );
+        wallLeftBottom.position = Vector3.MoveTowards(wallLeftBottom.position, wallLeftBottomSource.position, Time.deltaTime*fireWallMovementSpeed);
+        wallLeftTop.position = Vector3.MoveTowards(wallLeftTop.position, wallLeftTopSource.position, Time.deltaTime*fireWallMovementSpeed);
+        wallRightBottom.position = Vector3.MoveTowards(wallRightBottom.position, wallRightBottomSource.position, Time.deltaTime * fireWallMovementSpeed);
+        wallRightTop.position = Vector3.MoveTowards(wallRightTop.position, wallRightTopSource.position, Time.deltaTime * fireWallMovementSpeed);
+        wallTop.position = Vector3.MoveTowards(wallTop.position, wallTopSource.position , Time.deltaTime * fireWallMovementSpeed);
+        if (Mathf.Approximately(Vector3.Distance(wallBottom.position, wallBottomSource.position),0))
+        {
+            moveWallsOutside=false;
+            fireWallActive = false;
+        }
+
+
+        // shake
+        //wallBottom.GetComponent<ShakeMe>().Shake();
+        //wallLeftBottom.GetComponent<ShakeMe>().Shake();
+        //wallLeftTop.GetComponent<ShakeMe>().Shake();
+        //wallRightBottom.GetComponent<ShakeMe>().Shake();
+        //wallRightTop.GetComponent<ShakeMe>().Shake();
+        //wallTop.GetComponent<ShakeMe>().Shake();
     }
 
     private void UsePowerUp()
@@ -68,7 +163,9 @@ public class PowerUpManager : MonoBehaviour
 
     private void UseFireWalls()
     {
-        Debug.Log("Using Firewalls!");
+        fireWallActive = true;
+        moveWallsInside = true;
+        //StartCoroutine(WallMoveFunction(wallBottom.transform, wallBottomDestination));
     }
 
     void addPowerUp(PowerUpType type)
@@ -119,5 +216,13 @@ public class PowerUpManager : MonoBehaviour
         {
             addPowerUp(PowerUpType.Shield);
         }
+    }
+    // co routine to pause 5 second
+    IEnumerator Pause()
+    {
+        
+        yield return new WaitForSeconds(15);
+        moveWallsOutside = true;
+
     }
 }
