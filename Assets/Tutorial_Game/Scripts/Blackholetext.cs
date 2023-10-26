c8de2d6aab826b63ab3075ca3a75504571385406
rahul02122000 <ra2777@srmist.edu.in>
2023-10-25 20:03:31 -0700

    rahul commiting avoid blackhole

---
+++ Assets/Tutorial_Game/Scripts/Blackholetext.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class Blackholetext : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+
+    void Start()
+    {
+        if (textComponent != null)
+        {
+            textComponent.gameObject.SetActive(false);  // Hide the text initially
+            Invoke("ShowTextComponent", 5f);  // Schedule the text to be shown after 5 seconds
+        }
+        else
+        {
+            Debug.LogError("Text component not assigned!");
+        }
+    }
+
+    void ShowTextComponent()
+    {
+        textComponent.gameObject.SetActive(true);
+        Invoke("HideTextComponent", 5f);  // Schedule the text to be hidden after 5 seconds
+    }
+
+    void HideTextComponent()
+    {
+        textComponent.gameObject.SetActive(false);
+    }
+}
